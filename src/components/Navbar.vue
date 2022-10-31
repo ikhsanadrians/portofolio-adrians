1bd94218800a523102e1a62435e38546175a6f2e
ikhsanadrians <aikhsan032@gmail.com>
2022-11-01 02:38:03 +0700

    add id for every card skills

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,7 +10,7 @@
          <ul>
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
-            <li><a href="#skill">Skills</a></li>
+            <li><a href="#skill">Tools</a></li>
             <li><a href="#project">Projects</a></li>
             <li><a href="">Contact</a></li>
          </ul>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './scss/app.scss'
 import './index.css'
 import './app.js';
+import tippy from 'tippy.js';
 import App from './App.vue'
 
 createApp(App).mount('#app')
